93eea8cb924519fe1dfc2b267344911ad00b365d
Francisco Damião Terto <francisco.terto@amoedo.com.br>
2020-02-14 14:20:28 -0300

    Rotas para criaação e ateração de usuários criada, validação adicionada e autenticação JWT

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/auth/UserController';
+import SessionController from './app/controllers/auth/SessionController';
+
+import AuthMiddleware from './app/http/middlewares/authMiddleware';
+import authMiddleware from './app/http/middlewares/authMiddleware';
+
 const route = new Router();
 
-route.get('/teste', (req, res) => {
-  return res.json({ message: 'Bem-Vindo ao GoBarber !' });
+route.get('/', async (req, res) => {
+    return res.json({ message: 'Bem-Vindo a API do Gobarber' });
 });
+
+route.post('/user', UserController.store);
+route.post('/sessions', SessionController.store);
+
+route.use(authMiddleware);
+
+route.put('/user', UserController.update);
+
 
 export default route;
