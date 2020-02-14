b9b20ff48c162735ace88c6daaf4e86cb82ca7fc
Francisco Damião Terto <francisco.terto@amoedo.com.br>
2020-02-14 14:24:49 -0300

    configurações do bando de dados

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'pgsql',
+    password: 'pqsql',
     database: 'gobarber',
     define: {
         timestamps: true,
